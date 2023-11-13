f0deb97e0faef8bfdc6f8421bc7fa98f419c4ff3
Javier <javier.developer0@gmail.com>
2023-11-13 01:39:11 +0100

    Add IconProps and TitlesHeader types.

---
+++ src/types/icon.props.ts
@@ -0,0 +1,9 @@
+import type { MemoExoticComponent } from 'react'
+import type { Icons } from '@/enums/icons.enum'
+
+export interface IconProps {
+  Icon: MemoExoticComponent<() => JSX.Element>
+  IconActive: MemoExoticComponent<() => JSX.Element>
+  tag: Icons
+  isActive?: boolean
+}
---
+++ src/types/titles-header.type.ts
@@ -0,0 +1,1 @@
+export type TitlesHeader = 'inicio' | 'ejercicios' | 'perfil' | 'scan'
