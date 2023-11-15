39e0582384b4e3b41d73801eb7e5d0c623e56291
Javier <javier.developer0@gmail.com>
2023-11-15 01:32:48 +0100

    Add CloseIcon component to public/icons

--- public/icons/index.tsx
+++ public/icons/index.tsx
@@ -122,3 +122,16 @@ export const QrCodeActiveIcon = memo(() => (
     />
   </svg>
 ))
+
+export const CloseIcon = memo(() => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1.5}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M6 18L18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+))
