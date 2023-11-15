d03f5a28df942d8abd82563788f7f070fae79d3d
Javier <javier.developer0@gmail.com>
2023-11-15 01:34:38 +0100

    Remove link to scan page and update exercise registration button style.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 import { Container } from '@/components/container'
-import { ROUTES } from '@/constants/routes.consts'
 
 export default function Home(): JSX.Element {
   return (
     <main>
       <Container>
         <section className="flex flex-col gap-4">
-          <Link href={ROUTES.Scan}>
+          <button type="button">
             <figure className="relative w-full">
               <Image
                 priority
@@ -19,12 +17,11 @@ export default function Home(): JSX.Element {
                 src="/images/registra-tu-ejercicio.webp"
                 width={500}
               />
-              <div className="absolute bottom-3 left-4 text-white">
+              <div className="absolute bottom-3 left-4 text-left text-white">
                 <h2>Registra tu ejercicio</h2>
-                <p className="text-xs">Elige como quieres registrar tu ejercicio</p>
               </div>
             </figure>
-          </Link>
+          </button>
         </section>
       </Container>
     </main>
