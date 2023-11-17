07ece7e95dd52f062fdaf289130dde96445235a8
Javier <javier.developer0@gmail.com>
2023-11-17 01:57:57 +0100

    Update Icon component props.

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,16 +1,16 @@
-import type { IconProps } from '@/types/icon.props'
+import type { IconProps } from '@/types/props/icon.props'
 
 import { memo } from 'react'
 
 export const Icon = memo(({ Icon, tag, isActive, IconActive }: IconProps): JSX.Element => {
   return (
-    <span className="flex h-10 w-10 flex-col items-center justify-center text-xs text-[#5e5e5e]">
+    <span className="text-primary-color flex h-10 w-10 flex-col items-center justify-center text-xs">
       <span
         className={`h-[32px] w-[32px] rounded-md  p-1 ${
-          isActive ? 'bg-gray-100' : 'bg-transparent'
+          isActive ? 'bg-gray-200' : 'bg-transparent'
         }`}
       >
-        {isActive ? <IconActive /> : <Icon />}
+        {isActive && IconActive ? <IconActive /> : <Icon />}
       </span>
       <p>{tag}</p>
     </span>
