0e302b53af4a96781c05771bf5c32dd10790ec01
Javier <javier.developer0@gmail.com>
2023-11-17 01:57:28 +0100

    Refactor Home page to use HomeList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,11 @@
-import Image from 'next/image'
-
 import { Container } from '@/components/container'
+import { HomeList } from '@/components/home-list'
 
 export default function Home(): JSX.Element {
   return (
     <main>
       <Container>
-        <section className="flex flex-col gap-4">
-          <button type="button">
-            <figure className="relative w-full">
-              <Image
-                priority
-                alt="Photo by Victor Freitas on Unsplash - https://unsplash.com/photos/person-about-to-lift-the-barbel-WvDYdXDzkhs?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
-                className="h-auto w-full rounded-md object-cover brightness-75"
-                height={500}
-                src="/images/registra-tu-ejercicio.webp"
-                width={500}
-              />
-              <div className="absolute bottom-3 left-4 text-left text-white">
-                <h2>Registra tu ejercicio</h2>
-              </div>
-            </figure>
-          </button>
-        </section>
+        <HomeList />
       </Container>
     </main>
   )
