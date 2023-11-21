ac9f104ee0b54e9ea070a67f890fe9ab84816946
Javier <javier.developer0@gmail.com>
2023-11-21 20:01:53 +0100

    Refactor floating button component and add exercise form modal
    Signed-off-by: Javier <javier.developer0@gmail.com>

--- src/components/floatting-button.tsx
+++ src/components/floatting-button.tsx
@@ -1,65 +1,27 @@
 'use client'
 
-import { memo, useRef, useState } from 'react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { memo } from 'react'
 
-import { PlusIcon, ExerciceIcon, HomeIcon } from '@public/icons'
-import { ROUTES } from '@/constants/routes.const'
-import useOutsideClick from '@/hooks/use-outside-click'
+import { PlusIcon } from '@public/icons'
+import { useModal } from '@/hooks/use-modal'
 
 import { Icon } from './icon'
+import { FormExercise } from './modals/form-exercise'
 
 export const FloattingButton = memo((): JSX.Element => {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-  const pathname = usePathname()
-  const divRef = useRef<HTMLDivElement>(null)
-
-  useOutsideClick(divRef, () => {
-    setIsOpen(false)
-  })
+  const { openModalWithContent } = useModal()
 
   return (
-    <div ref={divRef}>
+    <div>
       <button
         className="fixed bottom-3 right-3 rounded-md border bg-white shadow-sm"
         type="button"
         onClick={() => {
-          setIsOpen(!isOpen)
+          openModalWithContent('Registrar ejercicio', <FormExercise />)
         }}
       >
         <Icon Icon={PlusIcon} />
       </button>
-
-      {isOpen ? (
-        <div className="fixed bottom-16 right-3 flex flex-col gap-2">
-          {pathname !== ROUTES.Exercises && (
-            <Link
-              className="rounded-md border bg-white shadow-sm"
-              href={ROUTES.Exercises}
-              type="button"
-              onClick={() => {
-                setIsOpen(false)
-              }}
-            >
-              <Icon Icon={ExerciceIcon} />
-            </Link>
-          )}
-
-          {pathname !== ROUTES.Home && (
-            <Link
-              className="rounded-md border bg-white shadow-sm"
-              href={ROUTES.Home}
-              type="button"
-              onClick={() => {
-                setIsOpen(false)
-              }}
-            >
-              <Icon Icon={HomeIcon} />
-            </Link>
-          )}
-        </div>
-      ) : null}
     </div>
   )
 })
