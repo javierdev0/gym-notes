6ea1d6e2c61502dd0824556fc92297de7f02a46e
Javier <javier.developer0@gmail.com>
2023-11-21 20:02:03 +0100

    Add floating button to container component
    Signed-off-by: Javier <javier.developer0@gmail.com>

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,6 +2,13 @@ import type { ReactNode } from 'react'
 
 import { memo } from 'react'
 
+import { FloattingButton } from './floatting-button'
+
 export const Container = memo(({ children }: { children: ReactNode }): JSX.Element => {
-  return <div className="w-screen max-w-md px-4 pt-4">{children}</div>
+  return (
+    <div className="relative mx-auto w-screen max-w-md px-4 pt-4">
+      {children}
+      <FloattingButton />
+    </div>
+  )
 })
