22b0d05246b9deb04bbd4294787adeace0add25c
Javier <javier.developer0@gmail.com>
2023-11-21 20:02:07 +0100

    Refactor home page to use ExerciseList component
    Signed-off-by: Javier <javier.developer0@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import type { Metadata } from 'next'
+
 import { Container } from '@/components/container'
-import { HomeList } from '@/components/home-list'
+import { ExerciseList } from '@/components/exercise-list'
+
+export const metadata: Metadata = {
+  title: 'Gymnotes - App',
+  description: 'App para registrar tus ejercicios de gimnasio'
+}
 
 export default function Home(): JSX.Element {
   return (
-    <main>
-      <Container>
-        <HomeList />
-      </Container>
-    </main>
+    <Container>
+      <ExerciseList />
+    </Container>
   )
 }
